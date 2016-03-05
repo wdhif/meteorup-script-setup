1e547193b972eb69d3701a4715c24e523779bcf2
Wassim Dhif <wassimdhif@gmail.com>
2016-03-05 22:38:02 +0100

    Password fix, root

--- mupsetup.sh
+++ mupsetup.sh
@@ -5,16 +5,16 @@ echo "This script is made to work on debian-like distributions"
 username="Creator4983"
 password="KevinFlynn"
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
 fi
 
 useradd $username
-echo $password | passwd $username --stdin
+echo $username:$password | chpasswd
 adduser $username sudo
-sed -i 's/# %sudo  ALL=(ALL) ALL/%sudo ALL=(ALL) NOPASSWD:ALL/'
-sed -i 's/%sudo  ALL=(ALL) ALL/%sudo ALL=(ALL) NOPASSWD:ALL/'
+sed -i 's/# %sudo  ALL=(ALL) ALL/%sudo ALL=(ALL) NOPASSWD:ALL/' /etc/sudoers
+sed -i 's/%sudo  ALL=(ALL) ALL/%sudo ALL=(ALL) NOPASSWD:ALL/' /etc/sudoers
 service sudo restart
 
 echo "Check the sudoers file with visudo for %sudo ALL=(ALL) NOPASSWD:ALL"
