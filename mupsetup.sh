504b2d64ecda74fa66edf2f4c924c2ad2971bd8d
Wassim Dhif <wassimdhif@gmail.com>
2016-03-06 11:37:49 +0100

    verifications of the sudoers file added

--- mupsetup.sh
+++ mupsetup.sh
@@ -4,6 +4,7 @@ echo "This script is made to work on debian-like distributions"
 
 username="Creator4983"
 password="KevinFlynn"
+sudoers="/etc/sudoers"
 
 if ! [ $(id -u) = 0 ]; then
     echo "Please run as root"
@@ -15,5 +16,10 @@ echo $username:$password | chpasswd
 adduser $username sudo
 echo "# meteorup-server-script\n%sudo ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 service sudo restart
+
+if grep -q "%sudo ALL=(ALL) NOPASSWD:ALL" $sudoers; then
+   echo "The script worked correctly, you can now use the new user in your mup.json"
+   exit
+fi
 
 echo "Check the sudoers file with visudo for %sudo ALL=(ALL) NOPASSWD:ALL"
